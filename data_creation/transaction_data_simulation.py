7dce371b6339d7ca6f56d903f376821623314517
Akhil K V <akhil2kv3697@gmail.com>
2022-10-11 23:50:27 +0530

    made few changes as "sonar fixes"

--- data_creation/transaction_data_simulation.py
+++ data_creation/transaction_data_simulation.py
@@ -36,31 +36,26 @@ sns.set_style('darkgrid', {'axes.facecolor': '0.9'})
 def generate_customer_profiles_table(n_customers, random_state=0):
     
     np.random.seed(random_state)
-        
+
     customer_id_properties=[]
-    
+
     # Generate customer properties from random distributions 
     for customer_id in range(n_customers):
-        
+
         x_customer_id = np.random.uniform(0,100)
         y_customer_id = np.random.uniform(0,100)
-        
+
         mean_amount = np.random.uniform(5,100) # Arbitrary (but sensible) value 
         std_amount = mean_amount/2 # Arbitrary (but sensible) value
-        
+
         mean_nb_tx_per_day = np.random.uniform(0,4) # Arbitrary (but sensible) value 
-        
+
         customer_id_properties.append([customer_id,
                                       x_customer_id, y_customer_id,
                                       mean_amount, std_amount,
                                       mean_nb_tx_per_day])
-        
-    customer_profiles_table = pd.DataFrame(customer_id_properties, columns=['CUSTOMER_ID',
-                                                                      'x_customer_id', 'y_customer_id',
-                                                                      'mean_amount', 'std_amount',
-                                                                      'mean_nb_tx_per_day'])
-    
-    return customer_profiles_table
+
+    return pd.DataFrame(customer_id_properties, columns=['CUSTOMER_ID', 'x_customer_id', 'y_customer_id', 'mean_amount', 'std_amount', 'mean_nb_tx_per_day'])
 
 # ****************************************************************
 # Terminal profiles generation
@@ -74,22 +69,19 @@ def generate_customer_profiles_table(n_customers, random_state=0):
 def generate_terminal_profiles_table(n_terminals, random_state=0):
     
     np.random.seed(random_state)
-        
+
     terminal_id_properties=[]
-    
+
     # Generate terminal properties from random distributions 
     for terminal_id in range(n_terminals):
-        
+
         x_terminal_id = np.random.uniform(0,100)
         y_terminal_id = np.random.uniform(0,100)
-        
+
         terminal_id_properties.append([terminal_id,
                                       x_terminal_id, y_terminal_id])
-                                       
-    terminal_profiles_table = pd.DataFrame(terminal_id_properties, columns=['TERMINAL_ID',
-                                                                      'x_terminal_id', 'y_terminal_id'])
-    
-    return terminal_profiles_table
+
+    return pd.DataFrame(terminal_id_properties, columns=['TERMINAL_ID', 'x_terminal_id', 'y_terminal_id'])
 
 # ****************************************************************
 # Association of customer profiles to terminals
@@ -101,21 +93,18 @@ def generate_terminal_profiles_table(n_terminals, random_state=0):
 def get_list_terminals_within_radius(customer_profile, x_y_terminals, r):
     
     # Use numpy arrays in the following to speed up computations
-    
+
     # Location (x,y) of customer as numpy array
     x_y_customer = customer_profile[['x_customer_id','y_customer_id']].values.astype(float)
-    
+
     # Squared difference in coordinates between customer and terminal locations
     squared_diff_x_y = np.square(x_y_customer - x_y_terminals)
-    
+
     # Sum along rows and compute suared root to get distance
     dist_x_y = np.sqrt(np.sum(squared_diff_x_y, axis=1))
-    
-    # Get the indices of terminals which are at a distance less than r
-    available_terminals = list(np.where(dist_x_y<r)[0])
-    
+
     # Return the list of terminal IDs
-    return available_terminals
+    return list(np.where(dist_x_y<r)[0])
 
 
 # ****************************************************************
@@ -254,51 +243,52 @@ def add_frauds(customer_profiles_table, terminal_profiles_table, transactions_df
     # By default, all transactions are genuine
     transactions_df['TX_FRAUD']=0
     transactions_df['TX_FRAUD_SCENARIO']=0
-    
+
     # Scenario 1
     transactions_df.loc[transactions_df.TX_AMOUNT>220, 'TX_FRAUD']=1
     transactions_df.loc[transactions_df.TX_AMOUNT>220, 'TX_FRAUD_SCENARIO']=1
     nb_frauds_scenario_1=transactions_df.TX_FRAUD.sum()
-    print("Number of frauds from scenario 1: "+str(nb_frauds_scenario_1))
-    
+    print(f"Number of frauds from scenario 1: {str(nb_frauds_scenario_1)}")
+
     # Scenario 2
     for day in range(transactions_df.TX_TIME_DAYS.max()):
-        
+
         compromised_terminals = terminal_profiles_table.TERMINAL_ID.sample(n=2, random_state=day)
-        
+
         compromised_transactions=transactions_df[(transactions_df.TX_TIME_DAYS>=day) & 
                                                     (transactions_df.TX_TIME_DAYS<day+28) & 
                                                     (transactions_df.TERMINAL_ID.isin(compromised_terminals))]
-                            
+
         transactions_df.loc[compromised_transactions.index,'TX_FRAUD']=1
         transactions_df.loc[compromised_transactions.index,'TX_FRAUD_SCENARIO']=2
-    
+
     nb_frauds_scenario_2=transactions_df.TX_FRAUD.sum()-nb_frauds_scenario_1
-    print("Number of frauds from scenario 2: "+str(nb_frauds_scenario_2))
-    
+    print(f"Number of frauds from scenario 2: {str(nb_frauds_scenario_2)}")
+
     # Scenario 3
     for day in range(transactions_df.TX_TIME_DAYS.max()):
-        
+
         compromised_customers = customer_profiles_table.CUSTOMER_ID.sample(n=3, random_state=day).values
-        
+
         compromised_transactions=transactions_df[(transactions_df.TX_TIME_DAYS>=day) & 
                                                     (transactions_df.TX_TIME_DAYS<day+14) & 
                                                     (transactions_df.CUSTOMER_ID.isin(compromised_customers))]
-        
+
         nb_compromised_transactions=len(compromised_transactions)
-        
-        
+
+
         random.seed(day)
-        index_fauds = random.sample(list(compromised_transactions.index.values),k=int(nb_compromised_transactions/3))
-        
+        index_fauds = random.sample(list(compromised_transactions.index.values), k=nb_compromised_transactions // 3)
+
+
         transactions_df.loc[index_fauds,'TX_AMOUNT']=transactions_df.loc[index_fauds,'TX_AMOUNT']*5
         transactions_df.loc[index_fauds,'TX_FRAUD']=1
         transactions_df.loc[index_fauds,'TX_FRAUD_SCENARIO']=3
-        
-                             
+
+
     nb_frauds_scenario_3=transactions_df.TX_FRAUD.sum()-nb_frauds_scenario_2-nb_frauds_scenario_1
-    print("Number of frauds from scenario 3: "+str(nb_frauds_scenario_3))
-    
+    print(f"Number of frauds from scenario 3: {str(nb_frauds_scenario_3)}")
+
     return transactions_df 
 
 
