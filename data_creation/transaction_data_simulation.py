19a28a10dd4bbc5e480faf1ee6369730c6936fe5
Akhil K V <akhil2kv3697@gmail.com>
2022-10-11 23:05:16 +0530

    working data generation

---
+++ data_creation/transaction_data_simulation.py
@@ -0,0 +1,224 @@
+# Necessary imports for this notebook
+import os
+import numpy as np
+import pandas as pd
+import datetime
+import time
+import random
+# For plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pandarallel import pandarallel
+
+# Initialization
+pandarallel.initialize()
+
+sns.set_style('darkgrid', {'axes.facecolor': '0.9'})
+
+# ****************************************************************
+# Customer profiles generation
+# Each customer will be defined by the following properties:
+
+# - CUSTOMER_ID: The customer unique ID
+# - (x_customer_id,y_customer_id): A pair of real coordinates (x_customer_id,y_customer_id) in a 100 * 100 grid,
+#    that defines the geographical location of the customer
+# - (mean_amount, std_amount): The mean and standard deviation of the transaction amounts for the customer, 
+#    assuming that the transaction amounts follow a normal distribution. The mean_amount will be drawn from a uniform distribution (5,100) and the std_amount will be set as the mean_amount divided by two.
+# - mean_nb_tx_per_day: The average number of transactions per day for the customer,
+#    assuming that the number of transactions per day follows a Poisson distribution. This number will be drawn from a uniform distribution (0,4).
+# - The generate_customer_profiles_table function provides an implementation for generating a table of customer profiles.
+#    It takes as input the number of customers for which to generate a profile and a random state for reproducibility.
+#    It returns a DataFrame containing the properties for each customer.
+# ****************************************************************
+
+def generate_customer_profiles_table(n_customers, random_state=0):
+    
+    np.random.seed(random_state)
+        
+    customer_id_properties=[]
+    
+    # Generate customer properties from random distributions 
+    for customer_id in range(n_customers):
+        
+        x_customer_id = np.random.uniform(0,100)
+        y_customer_id = np.random.uniform(0,100)
+        
+        mean_amount = np.random.uniform(5,100) # Arbitrary (but sensible) value 
+        std_amount = mean_amount/2 # Arbitrary (but sensible) value
+        
+        mean_nb_tx_per_day = np.random.uniform(0,4) # Arbitrary (but sensible) value 
+        
+        customer_id_properties.append([customer_id,
+                                      x_customer_id, y_customer_id,
+                                      mean_amount, std_amount,
+                                      mean_nb_tx_per_day])
+        
+    customer_profiles_table = pd.DataFrame(customer_id_properties, columns=['CUSTOMER_ID',
+                                                                      'x_customer_id', 'y_customer_id',
+                                                                      'mean_amount', 'std_amount',
+                                                                      'mean_nb_tx_per_day'])
+    
+    return customer_profiles_table
+
+# ****************************************************************
+# Terminal profiles generation
+# Each terminal will be defined by the following properties:
+
+# TERMINAL_ID: The terminal ID
+# (x_terminal_id,y_terminal_id): A pair of real coordinates (x_terminal_id,y_terminal_id) in a 100 * 100 grid, that defines the geographical location of the terminal
+# The generate_terminal_profiles_table function provides an implementation for generating a table of terminal profiles. It takes as input the number of terminals for which to generate a profile and a random state for reproducibility. It returns a DataFrame containing the properties for each terminal.
+# ****************************************************************
+
+def generate_terminal_profiles_table(n_terminals, random_state=0):
+    
+    np.random.seed(random_state)
+        
+    terminal_id_properties=[]
+    
+    # Generate terminal properties from random distributions 
+    for terminal_id in range(n_terminals):
+        
+        x_terminal_id = np.random.uniform(0,100)
+        y_terminal_id = np.random.uniform(0,100)
+        
+        terminal_id_properties.append([terminal_id,
+                                      x_terminal_id, y_terminal_id])
+                                       
+    terminal_profiles_table = pd.DataFrame(terminal_id_properties, columns=['TERMINAL_ID',
+                                                                      'x_terminal_id', 'y_terminal_id'])
+    
+    return terminal_profiles_table
+
+# ****************************************************************
+# Association of customer profiles to terminals
+# Let us now associate terminals with the customer profiles. In our design, customers can only perform transactions on terminals that are within a radius of r of their geographical locations.
+
+# Let us first write a function, called get_list_terminals_within_radius, which finds these terminals for a customer profile. The function will take as input a customer profile (any row in the customer profiles table), an array that contains the geographical location of all terminals, and the radius r. It will return the list of terminals within a radius of r for that customer.
+# *****************************************************************
+
+def get_list_terminals_within_radius(customer_profile, x_y_terminals, r):
+    
+    # Use numpy arrays in the following to speed up computations
+    
+    # Location (x,y) of customer as numpy array
+    x_y_customer = customer_profile[['x_customer_id','y_customer_id']].values.astype(float)
+    
+    # Squared difference in coordinates between customer and terminal locations
+    squared_diff_x_y = np.square(x_y_customer - x_y_terminals)
+    
+    # Sum along rows and compute suared root to get distance
+    dist_x_y = np.sqrt(np.sum(squared_diff_x_y, axis=1))
+    
+    # Get the indices of terminals which are at a distance less than r
+    available_terminals = list(np.where(dist_x_y<r)[0])
+    
+    # Return the list of terminal IDs
+    return available_terminals
+
+
+# ****************************************************************
+# Generation of transactions
+
+# The customer profiles now contain all the information that we require to generate transactions. 
+# The transaction generation will be done by a function generate_transactions_table that takes as input a customer profile, 
+# a starting date, and a number of days for which to generate transactions. It will return a table of transactions, 
+# which follows the format presented above (without the transaction label, which will be added in fraud scenarios generation).
+# ****************************************************************
+
+def generate_transactions_table(customer_profile, start_date = "2018-04-01", nb_days = 10):
+    
+    customer_transactions = []
+    
+    random.seed(int(customer_profile.CUSTOMER_ID))
+    np.random.seed(int(customer_profile.CUSTOMER_ID))
+    
+    # For all days
+    for day in range(nb_days):
+        
+        # Random number of transactions for that day 
+        nb_tx = np.random.poisson(customer_profile.mean_nb_tx_per_day)
+        
+        # If nb_tx positive, let us generate transactions
+        if nb_tx>0:
+            
+            for tx in range(nb_tx):
+                
+                # Time of transaction: Around noon, std 20000 seconds. This choice aims at simulating the fact that 
+                # most transactions occur during the day.
+                time_tx = int(np.random.normal(86400/2, 20000))
+                
+                # If transaction time between 0 and 86400, let us keep it, otherwise, let us discard it
+                if (time_tx>0) and (time_tx<86400):
+                    
+                    # Amount is drawn from a normal distribution  
+                    amount = np.random.normal(customer_profile.mean_amount, customer_profile.std_amount)
+                    
+                    # If amount negative, draw from a uniform distribution
+                    if amount<0:
+                        amount = np.random.uniform(0,customer_profile.mean_amount*2)
+                    
+                    amount=np.round(amount,decimals=2)
+                    
+                    if len(customer_profile.available_terminals)>0:
+                        
+                        terminal_id = random.choice(customer_profile.available_terminals)
+                    
+                        customer_transactions.append([time_tx+day*86400, day,
+                                                      customer_profile.CUSTOMER_ID, 
+                                                      terminal_id, amount])
+            
+    customer_transactions = pd.DataFrame(customer_transactions, columns=['TX_TIME_SECONDS', 'TX_TIME_DAYS', 'CUSTOMER_ID', 'TERMINAL_ID', 'TX_AMOUNT'])
+    
+    if len(customer_transactions)>0:
+        customer_transactions['TX_DATETIME'] = pd.to_datetime(customer_transactions["TX_TIME_SECONDS"], unit='s', origin=start_date)
+        customer_transactions=customer_transactions[['TX_DATETIME','CUSTOMER_ID', 'TERMINAL_ID', 'TX_AMOUNT','TX_TIME_SECONDS', 'TX_TIME_DAYS']]
+    
+    return customer_transactions  
+
+
+
+
+
+def generate_dataset(n_customers = 10000, n_terminals = 1000000, nb_days=90, start_date="2018-04-01", r=5):
+    
+    start_time=time.time()
+    customer_profiles_table = generate_customer_profiles_table(n_customers, random_state = 0)
+    print("Time to generate customer profiles table: {0:.2}s".format(time.time()-start_time))
+    
+    start_time=time.time()
+    terminal_profiles_table = generate_terminal_profiles_table(n_terminals, random_state = 1)
+    print("Time to generate terminal profiles table: {0:.2}s".format(time.time()-start_time))
+    
+    start_time=time.time()
+    x_y_terminals = terminal_profiles_table[['x_terminal_id','y_terminal_id']].values.astype(float)
+    customer_profiles_table['available_terminals'] = customer_profiles_table.apply(lambda x : get_list_terminals_within_radius(x, x_y_terminals=x_y_terminals, r=r), axis=1)
+    # With Pandarallel
+    #customer_profiles_table['available_terminals'] = customer_profiles_table.parallel_apply(lambda x : get_list_closest_terminals(x, x_y_terminals=x_y_terminals, r=r), axis=1)
+    customer_profiles_table['nb_terminals']=customer_profiles_table.available_terminals.apply(len)
+    print("Time to associate terminals to customers: {0:.2}s".format(time.time()-start_time))
+    
+    start_time=time.time()
+    transactions_df=customer_profiles_table.groupby('CUSTOMER_ID').apply(lambda x : generate_transactions_table(x.iloc[0], nb_days=nb_days)).reset_index(drop=True)
+    # With Pandarallel
+    #transactions_df=customer_profiles_table.groupby('CUSTOMER_ID').parallel_apply(lambda x : generate_transactions_table(x.iloc[0], nb_days=nb_days)).reset_index(drop=True)
+    print("Time to generate transactions: {0:.2}s".format(time.time()-start_time))
+    
+    # Sort transactions chronologically
+    transactions_df=transactions_df.sort_values('TX_DATETIME')
+    # Reset indices, starting from 0
+    transactions_df.reset_index(inplace=True,drop=True)
+    transactions_df.reset_index(inplace=True)
+    # TRANSACTION_ID are the dataframe indices, starting from 0
+    transactions_df.rename(columns = {'index':'TRANSACTION_ID'}, inplace = True)
+    
+    return (customer_profiles_table, terminal_profiles_table, transactions_df)
+
+
+(customer_profiles_table, terminal_profiles_table, transactions_df)=\
+    generate_dataset(n_customers = 5000, 
+                     n_terminals = 10000, 
+                     nb_days=183, 
+                     start_date="2018-04-01", 
+                     r=5)
+
+print(transactions_df.shape)
